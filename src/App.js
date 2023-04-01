8262e8d0c63ec765aeaee6a63e84c1376f187338
penguin311 <lst5561@naver.com>
2023-04-01 10:20:00 +0900

    ver01

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import RouterPage from './componets/RouterPage';
+
+function App() {
+  return (
+    <div className="App">
+      <RouterPage/>
+    </div>
+  );
+}
+
+export default App;
